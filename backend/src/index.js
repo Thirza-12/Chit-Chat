2e1661df39ecfece427a3702530e3b1cc15de7a3
Thirza-12 <thirzabehal@gmail.com>
2025-02-16 18:46:59 +0530

    limit of files changed

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,9 @@ app.use(cors({
     credentials: true
 }
 ));
-app.use(express.json());
+// app.use(express.json());
+app.use(express.json({ limit: "10mb" })); 
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
