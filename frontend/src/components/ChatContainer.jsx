e25c23e588fabec778dcdd6436f489a10128f635
Thirza-12 <thirzabehal@gmail.com>
2025-02-16 22:23:46 +0530

    Chat container updated

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,95 +1,3 @@
-// import { useChatStore } from "../store/useChatStore";
-// import { useEffect, useRef } from "react";
-
-// import ChatHeader from "./ChatHeader";
-// import MessageInput from "./MessageInput";
-// import MessageSkeleton from "./skeletons/MessageSkeleton";
-// import { useAuthStore } from "../store/useAuthStore";
-// import { formatMessageTime } from "../lib/utils";
-
-// const ChatContainer = () => {
-//   const {
-//     messages,
-//     getMessages,
-//     isMessagesLoading,
-//     selectedUser,
-//     subscribeToMessages,
-//     unsubscribeFromMessages,
-//   } = useChatStore();
-//   const { authUser } = useAuthStore();
-//   const messageEndRef = useRef(null);
-
-//   useEffect(() => {
-//     getMessages(selectedUser._id);
-
-//     subscribeToMessages();
-
-//     return () => unsubscribeFromMessages();
-//   }, [selectedUser._id, getMessages, subscribeToMessages, unsubscribeFromMessages]);
-
-//   useEffect(() => {
-//     if (messageEndRef.current && messages) {
-//       messageEndRef.current.scrollIntoView({ behavior: "smooth" });
-//     }
-//   }, [messages]);
-
-//   if (isMessagesLoading) {
-//     return (
-//       <div className="flex-1 flex flex-col overflow-auto">
-//         <ChatHeader />
-//         <MessageSkeleton />
-//         <MessageInput />
-//       </div>
-//     );
-//   }
-
-//   return (
-//     <div className="flex-1 flex flex-col overflow-auto">
-//       <ChatHeader />
-
-//       <div className="flex-1 overflow-y-auto p-4 space-y-4">
-//         {messages.map((message) => (
-//           <div
-//             key={message._id}
-//             className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"}`}
-//             ref={messageEndRef}
-//           >
-//             <div className=" chat-image avatar">
-//               <div className="size-10 rounded-full border">
-//                 <img
-//                   src={
-//                     message.senderId === authUser._id
-//                       ? authUser.profilePic || "/image.png"
-//                       : selectedUser.profilePic || "/image.png"
-//                   }
-//                   alt="profile pic"
-//                 />
-//               </div>
-//             </div>
-//             <div className="chat-header mb-1">
-//               <time className="text-xs opacity-50 ml-1">
-//                 {formatMessageTime(message.createdAt)}
-//               </time>
-//             </div>
-//             <div className="chat-bubble flex flex-col">
-//               {message.image && (
-//                 <img
-//                   src={message.image}
-//                   alt="Attachment"
-//                   className="sm:max-w-[200px] rounded-md mb-2"
-//                 />
-//               )}
-//               {message.text && <p>{message.text}</p>}
-//             </div>
-//           </div>
-//         ))}
-//       </div>
-
-//       <MessageInput />
-//     </div>
-//   );
-// };
-// export default ChatContainer;
 import { useChatStore } from "../store/useChatStore";
 import { useEffect, useRef, useState } from "react";
 
@@ -167,15 +75,20 @@ const ChatContainer = () => {
             </div>
             <div className="chat-bubble flex flex-col">
               {message.image && (
-                <img
-                  src={message.image}
-                  alt="Attachment"
-                  className="sm:max-w-[200px] rounded-md mb-2 cursor-pointer"
-                  onClick={() => {
-                    setSelectedImage(message.image);
-                    document.getElementById("image_modal").showModal();
-                  }}
-                />
+                <>
+                  {/* Set selected image and open modal */}
+                  <label
+                    htmlFor="image_modal"
+                    className="cursor-pointer"
+                    onClick={() => setSelectedImage(message.image)}
+                  >
+                    <img
+                      src={message.image}
+                      alt="Attachment"
+                      className="sm:max-w-[200px] rounded-md mb-2"
+                    />
+                  </label>
+                </>
               )}
               {message.text && <p>{message.text}</p>}
             </div>
@@ -185,17 +98,14 @@ const ChatContainer = () => {
 
       <MessageInput />
 
-      {/* Image Preview Modal */}
-      <dialog id="image_modal" className="modal">
+      {/* Checkbox-Based Image Preview Modal */}
+      <input type="checkbox" id="image_modal" className="modal-toggle" />
+      <div className="modal" role="dialog">
         <div className="modal-box">
-          <img src={selectedImage} alt="Preview" className="w-full rounded-lg" />
-          <div className="modal-action">
-            <form method="dialog">
-              <button className="btn">Close</button>
-            </form>
-          </div>
+          {selectedImage && <img src={selectedImage} alt="Preview" className="w-full rounded-lg" />}
         </div>
-      </dialog>
+        <label className="modal-backdrop" htmlFor="image_modal">Close</label>
+      </div>
     </div>
   );
 };
